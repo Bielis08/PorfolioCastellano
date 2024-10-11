991a8fb9df49a626c475470bf1f7e9281f60dbca
Bielis08 <bielmonteroroman08@gmail.com>
2024-10-11 10:37:02 +0200

    Pagina la celestina añadida

---
+++ src/lib/capitulos.ts
@@ -0,0 +1,22 @@
+export const capitulos = [
+    {
+        capitulo: '1',
+        resumen: 'El primer acto de La Celestina comienza con Calisto, un joven noble, que entra en el jardín de Melibea, una hermosa joven de quien queda perdidamente enamorado tras verla por primera vez. Calisto intenta seducirla, pero ella lo rechaza de manera firme. Desesperado por conquistarla, Calisto acude a su criado Sempronio, quien le sugiere que busquen la ayuda de una alcahueta, la vieja y astuta Celestina. Sempronio tiene una alianza secreta con Celestina, esperando beneficiarse de la relación entre Calisto y Melibea. Ambos planean aprovechar la riqueza de Calisto manipulando su amor y su desesperación.',
+    },
+    {
+        capitulo: '2',
+        resumen: 'Cuando Sempronio llega a la casa de Celestina, ella acepta el encargo con entusiasmo, ya que ve en esto una oportunidad para enriquecerse. Además, Celestina y Sempronio discuten cómo repartirse las ganancias que obtendrán de Calisto. En paralelo, otro de los criados de Calisto, Pármeno, que conoce bien las artimañas de Celestina y es más leal a su amo, intenta advertirle del peligro de confiar en ella. Sin embargo, Calisto, cegado por su amor, no presta atención a las advertencias de Pármeno y decide seguir adelante con el plan.',
+    },
+    {
+        capitulo: '3',
+        resumen: 'Durante su conversación, Celestina menciona el nombre de Calisto, lo que despierta el enojo de Melibea, ya que no quiere tener nada que ver con él. A pesar de esto, Celestina no se rinde y utiliza sus habilidades de persuasión para plantar la semilla de la duda en Melibea. Calisto, completamente cautivado por las palabras de Celestina, le entrega una generosa recompensa por sus esfuerzos. Mientras tanto, Sempronio observa todo con satisfacción, ya que él y Celestina tienen planes de aprovechar la situación para su propio beneficio. Pármeno, por su parte, sigue desconfiando de Celestina y se siente incómodo con la manipulación que está teniendo lugar. Sin embargo, la presión de Sempronio y las promesas de Celestina de incluirlo en las ganancias lo empiezan a hacer dudar. Así, poco a poco, Pármeno va cediendo y parece dispuesto a unirse a la conspiración.Finalmente, Celestina logra salir de la casa de Melibea con un pequeño avance: ha conseguido que Melibea acepte un cordón como símbolo, lo que ella interpreta como una señal de esperanza para su plan de unir a los amantes.',
+    },
+    {
+        capitulo: '4',
+        resumen: 'Calisto, completamente cautivado por las palabras de Celestina, le entrega una generosa recompensa por sus esfuerzos. Mientras tanto, Sempronio observa todo con satisfacción, ya que él y Celestina tienen planes de aprovechar la situación para su propio beneficio. Pármeno, por su parte, sigue desconfiando de Celestina y se siente incómodo con la manipulación que está teniendo lugar. Sin embargo, la presión de Sempronio y las promesas de Celestina de incluirlo en las ganancias lo empiezan a hacer dudar. Así, poco a poco, Pármeno va cediendo y parece dispuesto a unirse a la conspiración.',
+    },
+    {
+        capitulo: '5',
+        resumen: 'Mientras tanto, Calisto sigue completamente obsesionado con Melibea, ansiando verla y convencido de que, gracias a la intervención de Celestina, su amor será correspondido. Celestina mantiene esa ilusión en Calisto, asegurándole que Melibea ya está más receptiva. Sempronio y Pármeno, ahora en connivencia con Celestina, comienzan a esperar su parte de las ganancias que obtendrán de Calisto. Celestina se aprovecha de la codicia y el deseo de todos a su alrededor para controlarlos y mantenerlos en su órbita, mientras sigue adelante con su plan de unir a Calisto y Melibea para su propio beneficio personal.',
+    },
+]
--- src/lib/temas.ts
+++ src/lib/temas.ts
@@ -2,21 +2,21 @@ export const temas = [
     {
         titulo: 'Clases de Palabras',
         descripcion: 'La lengua se divide en diversos niveles que nos ayuda a ordenarla y que tienen relación entre ellos.',
-        icono: '/Gramatica.svg',
+        icono: '/abc.svg',
         iconColor: '#0EA5E9',
         link: '/clase_de_palabras'
     },
     {
         titulo: 'Literatura (Edad Media)',
         descripcion: 'La literatura de la Edad Media: Lírica Popular, El Mester de Juglaría y Mester de Clerecía.',
-        icono: '/Gramatica.svg',
+        icono: '/castillo.svg',
         iconColor: '#FACC15',
         link: '/literatura_edad_media'
     },
     {
         titulo: 'La Celestina (Capítulo V)',
         descripcion: 'La tragicomedia de Calisto y Melibea, una obra atribuida a Fernando de Rojas.',
-        icono: '/Gramatica.svg',
+        icono: '/book.svg',
         iconColor: '#9333EA',
         link: '/la_celestina'
     },
