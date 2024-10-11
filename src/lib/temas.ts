822bb9f7bab8994abf51e1ecdb44276a46d964e1
Bielis08 <bielmonteroroman08@gmail.com>
2024-10-11 13:42:28 +0200

    Responsive en todas las paginas

--- src/lib/temas.ts
+++ src/lib/temas.ts
@@ -2,21 +2,21 @@ export const temas = [
     {
         titulo: 'Clases de Palabras',
         descripcion: 'La lengua se divide en diversos niveles que nos ayuda a ordenarla y que tienen relación entre ellos.',
-        icono: '/abc.svg',
+        icono: '/icons/abc.svg',
         iconColor: '#0EA5E9',
         link: '/clase_de_palabras'
     },
     {
         titulo: 'Literatura (Edad Media)',
         descripcion: 'La literatura de la Edad Media: Lírica Popular, El Mester de Juglaría y Mester de Clerecía.',
-        icono: '/castillo.svg',
+        icono: '/icons/castillo.svg',
         iconColor: '#FACC15',
         link: '/literatura_edad_media'
     },
     {
         titulo: 'La Celestina (Capítulo V)',
         descripcion: 'La tragicomedia de Calisto y Melibea, una obra atribuida a Fernando de Rojas.',
-        icono: '/book.svg',
+        icono: '/icons/book.svg',
         iconColor: '#9333EA',
         link: '/la_celestina'
     },
