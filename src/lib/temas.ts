86ee75a12318b2a3fadb47f1c3bc62f6f7e9b519
Bielis08 <bielmonteroroman08@gmail.com>
2024-10-11 09:39:51 +0200

    Ahora los temas estan en un archivo aparte para acceder desde qualquier componente

---
+++ src/lib/temas.ts
@@ -0,0 +1,28 @@
+export const temas = [
+    {
+        titulo: 'Clases de Palabras',
+        descripcion: 'La lengua se divide en diversos niveles que nos ayuda a ordenarla y que tienen relación entre ellos.',
+        icono: '/Gramatica.svg',
+        iconColor: '#0EA5E9',
+        link: '/clase_de_palabras'
+    },
+    {
+        titulo: 'Literatura (Edad Media)',
+        descripcion: 'La literatura de la Edad Media: Lírica Popular, El Mester de Juglaría y Mester de Clerecía.',
+        icono: '/Gramatica.svg',
+        iconColor: '#FACC15',
+        link: '/literatura_edad_media'
+    },
+    {
+        titulo: 'La Celestina (Capítulo V)',
+        descripcion: 'La tragicomedia de Calisto y Melibea, una obra atribuida a Fernando de Rojas.',
+        icono: '/Gramatica.svg',
+        iconColor: '#9333EA',
+        link: '/la_celestina'
+    },
+]
+
+// Colors:
+//     gray-400 = #9CA3AF
+//     green-400 = #4ADE80
+//     pink-500 = #EC4899
