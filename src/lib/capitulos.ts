d9cec8cf6c2ad933e4c4cd061bdd6d6adc4e716c
Bielis08 <bielmonteroroman08@gmail.com>
2024-10-13 21:22:09 +0200

    otras mejoras

--- src/lib/capitulos.ts
+++ src/lib/capitulos.ts
@@ -1,15 +1,15 @@
 export const capitulos = [
     {
         capitulo: '1',
-        resumen: 'El primer acto de La Celestina comienza con Calisto, un joven noble, que entra en el jardín de Melibea, una hermosa joven de quien queda perdidamente enamorado tras verla por primera vez. Calisto intenta seducirla, pero ella lo rechaza de manera firme. Desesperado por conquistarla, Calisto acude a su criado Sempronio, quien le sugiere que busquen la ayuda de una alcahueta, la vieja y astuta Celestina. Sempronio tiene una alianza secreta con Celestina, esperando beneficiarse de la relación entre Calisto y Melibea. Ambos planean aprovechar la riqueza de Calisto manipulando su amor y su desesperación.',
+        resumen: 'Calisto, uno de los protagonistas, entra en el jardín de Melibea joven de la qual queda enamorado tras verla por primera vez. Calisto intenta seducirla, pero ella lo rechaza. Desesperado, Calisto acude a su criado Sempronio, quien le sugiere que busquen la ayuda de una alcahueta, la Celestina. Sempronio, tiene una relación oculta con la Celestina, es su madre, entonces Sempronio y la Celestina planean beneficiarse de la riqueza de Calisto aprovechandose de su desesperación por conquistar a Melibea.',
     },
     {
         capitulo: '2',
-        resumen: 'Cuando Sempronio llega a la casa de Celestina, ella acepta el encargo con entusiasmo, ya que ve en esto una oportunidad para enriquecerse. Además, Celestina y Sempronio discuten cómo repartirse las ganancias que obtendrán de Calisto. En paralelo, otro de los criados de Calisto, Pármeno, que conoce bien las artimañas de Celestina y es más leal a su amo, intenta advertirle del peligro de confiar en ella. Sin embargo, Calisto, cegado por su amor, no presta atención a las advertencias de Pármeno y decide seguir adelante con el plan.',
+        resumen: 'Calisto envía a Sempronio a meter prisa a la Celestina, y de mientras, Pármeno, que conoce bien las malas intenciones de la Celestina, intenta convencer a Calisto para que no siga con esto. Calisto lo ignora y ordena prepara y limpiar un caballo por si funcniona el plan o pasa por delante de casa de Melibea.',
     },
     {
         capitulo: '3',
-        resumen: 'Durante su conversación, Celestina menciona el nombre de Calisto, lo que despierta el enojo de Melibea, ya que no quiere tener nada que ver con él. A pesar de esto, Celestina no se rinde y utiliza sus habilidades de persuasión para plantar la semilla de la duda en Melibea. Calisto, completamente cautivado por las palabras de Celestina, le entrega una generosa recompensa por sus esfuerzos. Mientras tanto, Sempronio observa todo con satisfacción, ya que él y Celestina tienen planes de aprovechar la situación para su propio beneficio. Pármeno, por su parte, sigue desconfiando de Celestina y se siente incómodo con la manipulación que está teniendo lugar. Sin embargo, la presión de Sempronio y las promesas de Celestina de incluirlo en las ganancias lo empiezan a hacer dudar. Así, poco a poco, Pármeno va cediendo y parece dispuesto a unirse a la conspiración.Finalmente, Celestina logra salir de la casa de Melibea con un pequeño avance: ha conseguido que Melibea acepte un cordón como símbolo, lo que ella interpreta como una señal de esperanza para su plan de unir a los amantes.',
+        resumen: 'Semprono visita a su madre, la Celestina por orden de Calisto, el le explica que Calisto esta desesperado y ansioso por la tardanza de la Celestina, que eso entra en el plan de ellos dos, para sacar más benefício. Por otro lado, la Celestina, explica a Pármeno que es hijo de una amiga suya ya fallecida, Claudina (Intentando convencer a Pármeno para engañar a su amo).',
     },
     {
         capitulo: '4',
