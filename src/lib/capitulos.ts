010380ba067a6816676fe1fee6e3196a25ae81eb
Bielis08 <bielmonteroroman08@gmail.com>
2024-10-18 20:12:51 +0200

    Corrección de faltas

--- src/lib/capitulos.ts
+++ src/lib/capitulos.ts
@@ -1,22 +1,22 @@
 export const capitulos = [
     {
         capitulo: '1',
-        resumen: 'Calisto, entra en el jardín de Melibea joven de la qual queda enamorado tras verla por primera vez. Calisto intenta seducirla, pero ella lo rechaza. Desesperado, Calisto acude a su criado Sempronio, quien le sugiere que busquen la ayuda de una alcahueta, la Celestina. Pero Sempronio y Celestina planean beneficiarse de la riqueza de Calisto aprovechandose de su desesperación por conquistar a Melibea.',
+        resumen: 'Calisto, entra en el jardín de Melibea joven de la cual queda enamorado tras verla por primera vez. Calisto intenta seducirla, pero ella lo rechaza. Desesperado, Calisto acude a su criado Sempronio, quien le sugiere que busquen la ayuda de una alcahueta, la Celestina. Pero Sempronio y Celestina planean beneficiarse de la riqueza de Calisto aprovechándose de su desesperación por conquistar a Melibea.',
     },
     {
         capitulo: '2',
-        resumen: 'Calisto envía a Sempronio a meter prisa a la Celestina, y de mientras, Pármeno, que conoce bien las malas intenciones de la Celestina, intenta convencer a Calisto para que no siga con esto. Calisto lo ignora y ordena prepara y limpiar un caballo por si funcniona el plan o pasa por delante de casa de Melibea.',
+        resumen: 'Calisto envía a Sempronio a meter prisa a la Celestina, y de mientras, Pármeno, que conoce bien las malas intenciones de la Celestina, intenta convencer a Calisto para que no siga con esto. Calisto lo ignora y ordena prepara y limpiar un caballo por si funciona el plan o pasa por delante de casa de Melibea.',
     },
     {
         capitulo: '3',
-        resumen: 'Semprono visita a su madre, la Celestina por orden de Calisto, el le explica que Calisto esta desesperado y ansioso por la tardanza de la Celestina, que eso entra en el plan de ellos dos, para sacar más benefício. Por otro lado, la Celestina, explica a Pármeno que es hijo de una amiga suya ya fallecida, Claudina (Intentando convencer a Pármeno para engañar a su amo).',
+        resumen: 'Sempronio visita a su madre, la Celestina por orden de Calisto, él le explica que Calisto está desesperado y ansioso por la tardanza de la Celestina, que eso entra en el plan de ellos dos, para sacar más beneficio. Por otro lado, la Celestina, explica a Pármeno que es hijo de una amiga suya ya fallecida, Claudina (Intentando convencer a Pármeno para engañar a su amo).',
     },
     {
         capitulo: '4',
-        resumen: 'Celestina finalmente va a visitar a Melibea, pero antes se encuentra con Lucrecia y Alisa que la reciben. Finalmente, cunado Celestina consigue quedarse a solas con Melibea saca el tema de Calisto, pero al momento Melibea empieza a decir lo mal que le cae y que no le gusta nada. Celestina no tiene otro remedio que hehcizarla, con un hilo al que hace estirar a Melibe la cual queda enamorada de Calisto.',
+        resumen: 'Celestina finalmente va a visitar a Melibea, pero antes se encuentra con Lucrecia y Alisa que la reciben. Finalmente, cuando Celestina consigue quedarse a solas con Melibea saca el tema de Calisto, pero al momento Melibea empieza a decir lo mal que le cae y que no le gusta nada. Celestina no tiene otro remedio que hechizarla, con un hilo al que hace estirar a Melibea la cual queda enamorada de Calisto.',
     },
     {
         capitulo: '5',
-        resumen: 'Por último, Celestina vuelve de casa de Melibea y se encuantra a Sempronio esparandola en su casa, de camino a casa de Calisto, Celestina le cuneta como ha ido. Cunado ya están llegando Pármeno los ve y avisa a Calisto, quien espera ansioso su llegada.',
+        resumen: 'Por último, Celestina vuelve de casa de Melibea y se encuentra a Sempronio esperándola en su casa, de camino a casa de Calisto, Celestina le cuenta como ha ido. Cunado ya están llegando Pármeno los ve y avisa a Calisto, quien espera ansioso su llegada.',
     },
 ]
