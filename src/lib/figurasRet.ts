3371d131b50ec270b837fef318583f10f60df06f
Bielis08 <bielmonteroroman08@gmail.com>
2024-11-09 12:26:34 +0100

    Figuras retoricas añadidas

---
+++ src/lib/figurasRet.ts
@@ -0,0 +1,97 @@
+export const figurasRetoricas = [
+    {
+        titulo: 'Aliteración',
+        descripcion: 'Repetición de uno o varios sonidos, sobre todo consonánticos, con una finalidad expresiva.',
+        ejemplo: 'Con el ala leve del leve abanico'
+    },
+    {
+        titulo: 'Onomatopeya',
+        descripcion: 'Aliteración que reproduce un sonido de la naturaleza.',
+        ejemplo: 'Un no sé qué que quedan balbuciendo'
+    },
+    {
+        titulo: 'Paranomasia',
+        descripcion: 'Empleo de vocablos de significados diferentes, pero de formas parecidas.',
+        ejemplo: 'Vendado que me has vendido'
+    },
+    {
+        titulo: 'Epíteto',
+        descripcion: 'Adjetivo calificativo que acompaña a un sustantivo, destacando una cualidad con la cual lo asociamos permanentemente.',
+        ejemplo: 'Blanca nieve'
+    },
+    {
+        titulo: 'Anáfora',
+        descripcion: 'Repetición de una o más palabras al principio de varios versos o grupos sintácticos.',
+        ejemplo: '¿Para quién edifiqué torres? / ¿Para quién adquirí honras? / ¿Para quién planté árboles?'
+    },
+    {
+        titulo: 'Anadiplosis',
+        descripcion: 'Repetición del final de un verso al principio del siguiente.',
+        ejemplo: 'Pasar haciendo caminos, / caminos sobre la mar'
+    },
+    {
+        titulo: 'Polisíndeton',
+        descripcion: 'Empleo de más conjunciones de las necesarias, dotando a la frase de mayor solemnidad y lentitud.',
+        ejemplo: 'Y llegué, y vi de lo que se trataba, y me marché sin decir nada'
+    },
+    {
+        titulo: 'Perífrasis',
+        descripcion: 'Expresar con varias palabras lo que se puede decir con un discurso más reducido.',
+        ejemplo: 'Iremos a la ciudad de los cármenes (=Granada)'
+    },
+    {
+        titulo: 'Elipsis',
+        descripcion: 'Supresión de algún término sin que se altere la comprensión del texto.',
+        ejemplo: ' Por una mirada, un mundo; / por una sonrisa, un cielo...'
+    },
+    {
+        titulo: 'Asíndeton',
+        descripcion: 'Supresión o ausencia total de conjunciones, produciendo rapidez y movimiento acelerado.',
+        ejemplo: 'Rendí, rompí, derribé / rajé, deshice, prendí...'
+    },
+    {
+        titulo: 'Hipérbaton',
+        descripcion: 'Alteración del orden lógico de la oración.',
+        ejemplo: 'Caído se le ha un clavel / hoy a la Aurora del seno'
+    },
+    {
+        titulo: 'Paralelismo',
+        descripcion: 'Repetición de construcciones similares en dos o más versos o grupossintácticos.',
+        ejemplo: 'La tierra más verde de huertos, / la tierra más rubia de mies...'
+    },
+    {
+        titulo: 'Apóstrofe',
+        descripcion: 'Interpelación exclamativa a un ser que no es el destinatario del discurso',
+        ejemplo: 'Para y óyeme ¡oh Sol!, yo te saludo'
+    },
+    {
+        titulo: 'Interrogación Retórica',
+        descripcion: 'Pregunta de la que no se espera respuesta',
+        ejemplo: 'Mi juventud... ¿qué juventud la mía?'
+    },
+    {
+        titulo: 'Símil o Comparación',
+        descripcion: 'Comparación de dos términos, uno real y otro imaginario, que poseen semejantes cualidades.',
+        ejemplo: 'Busco el descanso, como busca / el mar el agua del reguero'
+    },
+    {
+        titulo: 'Metáfora',
+        descripcion: 'Identificación de dos términos, uno real y otro imaginario, que poseen semejantes cualidades.',
+        ejemplo: 'Las casa son ojos'
+    },
+    {
+        titulo: 'Antítesis o Contraste',
+        descripcion: 'Contraposición de dostérminos o ideas contrarios',
+        ejemplo: 'Es hielo abrasador, es fuego helado'
+    },
+    {
+        titulo: 'Hipérbole',
+        descripcion: 'Exageración o desproporción de lo enunciado',
+        ejemplo: 'Tanto dolor se agrupa en mi costado/ que por doler me duele hasta el aliento'
+    },
+    {
+        titulo: 'Personificación',
+        descripcion: 'Atribución de cualidades humanas a seres animados o inanimados.',
+        ejemplo: 'Ríe la luminosa hora de primavera'
+    },
+]
