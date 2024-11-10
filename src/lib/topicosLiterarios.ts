9f619e4a1f69eadaf263d95f5b124f184af9013a
Bielis08 <bielmonteroroman08@gmail.com>
2024-11-10 12:28:45 +0100

    Añadido lib de topicos literarios

---
+++ src/lib/topicosLiterarios.ts
@@ -0,0 +1,50 @@
+export const topicosLiterarios = [
+    {
+        topico: 'Tempus fugit',
+        descripcion: 'El tiempo vuela y es efímero.'
+    },
+    {
+        topico: 'Memento mori',
+        descripcion: 'Recuerda que morirás, reflexiona sobre la mortalidad.'
+    },
+    {
+        topico: 'Aurea mediocritas',
+        descripcion: 'La felicidad está en la en una vida simple.'
+    },
+    {
+        topico: 'Contemptus mundi',
+        descripcion: 'Desprecio por lo material, se enfoca en lo espiritual.'
+    },
+    {
+        topico: 'Vita flumen',
+        descripcion: 'La vida es como un río, fluye y cambia constantemente.'
+    },
+    {
+        topico: 'Ubi sunt?',
+        descripcion: '¿Dónde están? Reflexiona sobre la fugacidad de la vida.'
+    },
+    {
+        topico: 'Omnia mors aequat',
+        descripcion: 'La muerte es iguala para todos, sin importar la riqueza.'
+    },
+    {
+        topico: 'Vita somnium',
+        descripcion: 'La vida es como un sueño, irreal y pasajera.'
+    },
+    {
+        topico: 'Non omnis moriar',
+        descripcion: 'No moriras del todo, una parte tuya permanecerá para siempre.'
+    },
+    {
+        topico: 'Homo Viator',
+        descripcion: 'El hombre como viajero, en un viaje simbólico de la vida.'
+    },
+    {
+        topico: 'Quotidie morimur',
+        descripcion: 'Morimos cada día, cada día estamos más cerca de la muerte.'
+    },
+    {
+        topico: 'Ontemptus mundi',
+        descripcion: 'La muerte nos iguala a todos.'
+    },
+];
