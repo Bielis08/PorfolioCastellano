fbed1a315898840bf6d465f47b50c870cdf27d81
Bielis08 <bielmonteroroman08@gmail.com>
2025-05-05 20:07:12 +0200

    Añade nuevo ícono SVG para "Lecturas" y actualiza la lista de temas con nuevos campos de tema en temas.ts

--- src/lib/temas.ts
+++ src/lib/temas.ts
@@ -4,62 +4,87 @@ export const temas = [
         descripcion: 'La lengua se divide en diversos niveles que nos ayudan a ordenarla y que tienen relación entre ellos.',
         icono: '/icons/abc.svg',
         iconColor: '#0EA5E9',
-        link: '/categorias_gramaticales'
+        link: '/categorias_gramaticales',
+        tema: '1'
     },
     {
         titulo: 'Literatura (Edad Media)',
         descripcion: 'La literatura de la Edad Media: Lírica Popular, El Mester de Juglaría y Mester de Clerecía.',
         icono: '/icons/castillo.svg',
         iconColor: '#FACC15',
-        link: '/literatura_edad_media'
+        link: '/literatura_edad_media',
+        tema: '1'
     },
     {
         titulo: 'Jorge Manrique',
         descripcion: 'Jorge Manrique, conocido por su obra "Coplas por la Muerte de su Padre".',
         icono: '/icons/author.svg',
         iconColor: '#4ADE80',
-        link: '/jorge_manrique'
+        link: '/jorge_manrique',
+        tema: '2'
     },
     {
         titulo: 'Argumentos y Adjuntos',
         descripcion: 'Gramatica de la segunda unidad, los complementos del verbo.',
         icono: '/icons/gramatica2.svg',
         iconColor: '#EC4899',
-        link: '/argumentos_adjuntos'
+        link: '/argumentos_adjuntos',
+        tema: '2'
+    },
+    {
+        titulo: 'Lecturas',
+        descripcion: 'Las dos lecturas del curso: "La Celestina" y "El Lazarillo de Tormes".',
+        icono: '/icons/libro.svg',
+        iconColor: '#4A8E80',
+        link: '/lecturas_trimestrales',
+        tema: '2 y 4'
     },
     {
         titulo: 'Literatura Renacimiento',
         descripcion: 'La literatura del Renacimiento: Garcilaso de la Vega, Fray Luis de León y San Juan de la Cruz.',
         icono: '/icons/renacimiento.svg',
         iconColor: '#D95700',
-        link: '/renacimiento'
+        link: '/renacimiento',
+        tema: '3 y 6'
     },
     {
         titulo: 'Perífrasis Verbales y Locuciones',
         descripcion: 'Gramática de la tercera unidad, las perífrasis verbales y las locuciones verbales.',
         icono: '/icons/gramatica2.svg',
         iconColor: '#FF1F17',
-        link: '/perifrasis_y_locuciones'
+        link: '/perifrasis_y_locuciones',
+        tema: '3'
     },
     {
         titulo: 'La Composición de las Palabras',
         descripcion: 'La composición de las palabras: prefijos, sufijos, raíces, cultas, patrimoniales...',
         icono: '/icons/tipografia.svg',
         iconColor: '#681CFF',
-        link: '/composicion_de_palabras'
+        link: '/composicion_de_palabras',
+        tema: '4'
     },
     {
         titulo: 'Literatura Barroco',
-        descripcion: 'Literatura del Barroco: Luis de Góngora, Francisco de Quevedo y Lope de Vega.',
+        descripcion: 'Luis de Góngora, Francisco de Quevedo y Lope de Vega.',
         icono: '/icons/barroco.svg',
         iconColor: '#991B1B',
-        link: '/literatura_barroco'
+        link: '/literatura_barroco',
+        tema: '5'
     },
     {
         titulo: 'El léxico y su semántica',
-        descripcion: 'El léxico y su semántica: el significado de las palabras, los campos semánticos y los campos léxicos.',
+        descripcion: 'El significado de las palabras, los campos semánticos y los campos léxicos.',
         icono: '/icons/semantica.svg',
         iconColor: '#FBBF24',
-        link: '/el_lexico_y_su_semantica'
+        link: '/el_lexico_y_su_semantica',
+        tema: '5'
+    },
+    {
+        titulo: 'Valores del se',
+        descripcion: 'Los valores del se: Variante del le, Relfexivo, Recíproco, Impersonal, Dativo...',
+        icono: '/icons/tipos_del_se.svg',
+        iconColor: '#F472B6',
+        link: '/tipos_del_se',
+        tema: '6'
     },
 ]
